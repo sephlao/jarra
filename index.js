0b3edfcb384595da37d739c37caa23f51353c394
josephlaotzu <josephlao.1398@gmail.com>
2020-01-28 21:08:56 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import Express from 'express';
+import HandleBars from 'express-handlebars';
+
+const app = new Express();
+
+app.engine('handlebars', new HandleBars());
+app.set('view engine', 'handlebars');
+
+app.use(Express.static('public'))
+
+app.listen(3000, () => {
+    console.log('web app is up and running...');
+})
+
+app.get('/', (req, res) => {
+    const data = {
+        title: 'Home',
+        text: 'Hello world this is my home page!'
+    }
+    res.render('home', data);
+})
