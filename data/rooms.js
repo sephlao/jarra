5e16d4ae50379fde91a5822ecf4b301f15ee18f2
Joseph Lao <n01370825@nj-00118-016.humber.org>
2020-02-10 17:08:01 -0500

    update room data

--- data/rooms.js
+++ data/rooms.js
@@ -3,93 +3,63 @@ module.exports = {
     error: { code: null, message: null },
     data: [{
         id: 1,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        name: 'Vinci Community',
+        location: 'Toronto Ontario, Canada',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 125,
-        rating: 4,
+        rating: 5,
         imgURL: 'https://a0.muscache.com/4ea/air/v2/pictures/1e3884d4-d4e9-442b-99c8-040a027d34c2.jpg',
-        tag: null
-    }, {
+        tag: null,
+        guest: 2
+    },  {
         id: 2,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        name: 'Iron Horse Towers ',
+        location: 'Kitchener Ontario, Canada',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 122,
-        rating: 4,
+        rating: 5,
         imgURL: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1873&q=80',
-        tag: null
+        tag: null,
+        guest: 2
     }, {
         id: 3,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        name: 'Chedoke Apartments',
+        location: 'Hamilton Ontario, Canada',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 126,
-        rating: 4,
+        rating: 4.5,
         imgURL: 'https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-        tag: null
+        tag: null,
+        guest: 2
     }, {
         id: 4,
         name: 'Lorem Ipsum',
         location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 128,
         rating: 4,
         imgURL: 'https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2106&q=80',
-        tag: null
+        tag: null,
+        guest: 2
     }, {
         id: 5,
         name: 'Lorem Ipsum',
         location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 135,
         rating: 4,
         imgURL: 'https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2784&q=80',
-        tag: null
+        tag: null,
+        guest: 2
     }, {
         id: 6,
         name: 'Lorem Ipsum',
         location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quod assumenda accusamus veritatis vitae...',
         price: 225,
         rating: 4,
         imgURL: 'https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80',
-        tag: null
-    }, {
-        id: 7,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
-        price: 155,
-        rating: 4,
-        imgURL: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        tag: null
-    }, {
-        id: 8,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
-        price: 125,
-        rating: 4,
-        imgURL: 'https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        tag: null
-    }, {
-        id: 9,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
-        price: 125,
-        rating: 4,
-        imgURL: 'https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80',
-        tag: null
-    }, {
-        id: 10,
-        name: 'Lorem Ipsum',
-        location: 'Missisauga, ON',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
-        price: 125,
-        rating: 4,
-        imgURL: 'https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80',
-        tag: null
+        tag: null,
+        guest: 2
     }]
 }
