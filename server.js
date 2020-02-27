47d94adcffea06aa500c879b8ba0ba9aa40cb4d4
josephlaotzu <josephlao.1398@gmail.com>
2020-02-27 16:55:16 -0500

    added twilio sms service on registration

--- server.js
+++ server.js
@@ -47,15 +47,3 @@ app.use((req, res) => {
 	const data = { url: req.originalUrl, user: getUserInfo() };
 	res.status(404).render('404', data);
 });
-
-// const accountSid = 'your_account_sid';
-// const authToken = 'your_auth_token';
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+15017122661',
-//      to: '+15558675310'
-//    })
-//   .then(message => console.log(message.sid));
