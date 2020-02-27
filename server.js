289f19f90886cfa00d8f202c3cbbfccc33b675fc
josephlaotzu <josephlao.1398@gmail.com>
2020-02-26 21:57:15 -0500

    added eslint and prettier dev dependencies

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// import Express from 'express';
-// import HandleBars from 'express-handlebars';
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -9,11 +7,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const getHomeData = () => ({
-    title: 'Home',
-    rooms: ROOMS.data.slice(0, 3),
-    user: app.get('currentUser')
+  title: 'Home',
+  rooms: ROOMS.data.slice(0, 3),
+  user: app.get('currentUser')
 });
-
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
@@ -22,61 +19,71 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
-    console.log(`Web app is up and running on http://localhost:${PORT}`);
+  console.log(`Web app is up and running on http://localhost:${PORT}`);
 });
 
 app.get('/', (req, res) => {
-    res.render('home', { ...getHomeData() });
+  res.render('home', { ...getHomeData() });
 });
 
 app.get('/rooms', (req, res) => {
-    res.render('rooms', { rooms: ROOMS.data, user: app.get('currentUser') });
+  res.render('rooms', { rooms: ROOMS.data, user: app.get('currentUser') });
 });
 
-app.route('/register').get((req, res) => {
+app
+  .route('/register')
+  .get((req, res) => {
     res.render('register');
-}).post(({ body: { firstname, lastname, bday, username, password } }, res) => {
+  })
+  .post(({ body: { firstname, lastname, bday, username, password } }, res) => {
     if (username && password) {
-        app.set('currentUser', { username, password, logged: true });
-        res.redirect('/');
+      app.set('currentUser', { username, password, logged: true });
+      res.redirect('/');
     } else {
-        res.render('register', {
-            error: {
-                firstname: !firstname,
-                lastname: !lastname,
-                bday: !bday,
-                username: !username,
-                password: !password,
-                passwordMatch: !(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password))
-            }, data: { firstname, lastname, bday, username, password }
-        })
+      res.render('register', {
+        error: {
+          firstname: !firstname,
+          lastname: !lastname,
+          bday: !bday,
+          username: !username,
+          password: !password,
+          passwordMatch: !/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(
+            password
+          )
+        },
+        data: { firstname, lastname, bday, username, password }
+      });
     }
-});
+  });
 
-app.route('/login').get((req, res) => {
+app
+  .route('/login')
+  .get((req, res) => {
     res.render('login');
-}).post(({ body: { username, password } }, res) => {
+  })
+  .post(({ body: { username, password } }, res) => {
     if (username && password) {
-        app.set('currentUser', { username, password, logged: true });
-        res.redirect('/');
+      app.set('currentUser', { username, password, logged: true });
+      res.redirect('/');
     } else {
-        res.render('login', {
-            error: {
-                username: !username,
-                password: !password
-            }, data: { username, password }
-        })
+      res.render('login', {
+        error: {
+          username: !username,
+          password: !password
+        },
+        data: { username, password }
+      });
     }
-});
+  });
 
 app.get('/logout', (req, res) => {
-    app.set('currentUser', {});
-    res.redirect('/')
-})
+  app.set('currentUser', {});
+  res.redirect('/');
+});
 
 app.use((req, res) => {
-    const data = { url: req.originalUrl, user: app.get('currentUser') };
-    res.status(404).render('404', data);
+  const data = { url: req.originalUrl, user: app.get('currentUser') };
+  res.status(404).render('404', data);
 });
 
 // const accountSid = 'your_account_sid';
