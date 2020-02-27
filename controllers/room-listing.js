0fb68651fd54c317fc69165acc51351fd6c76dbf
josephlaotzu <josephlao.1398@gmail.com>
2020-02-27 15:15:11 -0500

    fix user info on room listing

--- controllers/room-listing.js
+++ controllers/room-listing.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const roomsModel = require('../models/rooms');
+const { getUserInfo } = require('../models/user');
 
 const router = express.Router();
 const { getAllProducts } = roomsModel();
 
 router.get('/', (req, res) => {
-  res.render('rooms', {
-    title: 'Room Listing',
-    rooms: getAllProducts(),
-    user: router.get('currentUser')
-  });
+	res.render('rooms', {
+		title: 'Room Listing',
+		rooms: getAllProducts(),
+		user: getUserInfo()
+	});
 });
 
 module.exports = router;
